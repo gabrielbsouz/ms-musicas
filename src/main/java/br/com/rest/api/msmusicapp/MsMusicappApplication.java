64bc599749b1ef35e9bfbd18334a0ecaec3b5421
Gabriel Souza <gabriel.souza@aisdigital.com.br>
2020-04-23 13:07:29 -0300

    :tada: Criação do projeto
    Criação do Projeto

---
+++ src/main/java/br/com/rest/api/msmusicapp/MsMusicappApplication.java
@@ -0,0 +1,13 @@
+package br.com.rest.api.msmusicapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsMusicappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsMusicappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rest/api/msmusicapp/MsMusicappApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rest.api.msmusicapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsMusicappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
