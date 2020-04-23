074448af60e2063896d3ebaea5c3fa1b67b72205
Gabriel Souza <gabriel.souza@aisdigital.com.br>
2020-04-23 18:39:19 -0300

    :sparkles: Implementação do cadastro de Músicas
    Implementação do cadastro de Músicas

--- src/main/java/br/com/rest/api/msmusicapp/mappers/MusicaMapper.java
+++ src/main/java/br/com/rest/api/msmusicapp/mappers/MusicaMapper.java
@@ -1,7 +1,9 @@
 package br.com.rest.api.msmusicapp.mappers;
 
 import br.com.rest.api.msmusicapp.models.Music;
+import io.swagger.model.DetalheMusica;
 import io.swagger.model.MusicaArray;
+import io.swagger.model.MusicaPostRequest;
 import org.mapstruct.Mapper;
 
 import java.util.List;
@@ -10,4 +12,8 @@ import java.util.List;
 public interface MusicaMapper {
 
     MusicaArray listMusicTo(List<Music> musicList);
+
+    Music musicFormPostToMusic(MusicaPostRequest musicaPostRequest);
+
+    DetalheMusica musicToDetalheMusica(Music music);
 }
--- src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO MUSIC (CD_MUSICA, NM_MUSICA, NM_ARTISTA , NM_GENERO ,DT_LANCAMENTO)
-VALUES ('10efb9aa-9cd2-442b-8ac9-ef75e1fd9ef1', 'A gente fez amor', 'Gusttavo Lima', 'Sertanejo', '2019-06-12');
